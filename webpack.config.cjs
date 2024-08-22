5fd23c0b3965050c9284f6ed4f2893968d30bd35
Downey <devops9204@gmail.com>
2024-08-22 23:11:52 +0900

    chore: Add public/, src/ and set up basic(babel) webpack config

---
+++ webpack.config.cjs
@@ -0,0 +1,50 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = (env) => {
+  const isDevMode = env.mode === 'development';
+
+  return {
+    mode: isDevMode ? 'development' : 'production',
+    devtool: isDevMode ? 'inline-soure-map' : false,
+    entry: { main: './src/index.tsx' },
+    output: {
+      publicPath: '/',
+      path: path.join(__dirname, 'dist'),
+      filename: './static/js/[name].[contenthash:8].bundle.js',
+      clean: true,
+    },
+    resolve: {
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      alias: {
+        '@src': path.join(__dirname, 'src'),
+      },
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(ts|tsx)$/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                [
+                  '@babel/preset-env',
+                  { targets: 'defaults', useBuiltIns: 'usage', corejs: 3 },
+                ],
+                ['@babel/preset-react', { runtime: 'automatic' }],
+                ['@babel/preset-typescript'],
+              ],
+            },
+          },
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+      }),
+    ],
+  };
+};
